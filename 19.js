86e7f90891148cfcebde45109d459e85bd6d5bfb
lijiahao <lijiahao@keep.com>
2020-06-11 17:07:01 +0800

    polish leetcode 19

--- 19.js
+++ 19.js
@@ -11,40 +11,24 @@
  * @return {ListNode}
  */
 var removeNthFromEnd = function(head, n) {
-  var len = 1,
-      arr = [],
-      newHead = null,
-      tempHead = null
-  
-  while (head !== null) {
-    len++
-    arr.push(head.val)
+  const header = head
+  const countLength = (head) => {
+    let length = 1
+    while (head.next) {
+      length++
+      head = head.next
+    }
+    return length
+  }
+  const length = countLength(head)
+
+  if (n === length) return null
+
+  let flag = 0
+  if (length - n  > flag && head.next) {
     head = head.next
   }
 
-  arr.splice(len - n - 1, 1)
-
-  arr.forEach((item, index) => {
-    const tempPtr = new ListNode(item)
-    if (index === 0) {
-      newHead = tempHead = tempPtr
-    } else {
-      tempHead.next = tempPtr
-      tempHead = tempHead.next
-    }
-  })
-
-  return newHead
-};
-
-// var removeNthFromEnd = function(head, n) {
-//   var left, before, right = head;
-//   left = before = {next: head}; 
-//   while (n--) right = right.next;
-//   while (right) {
-//     right = right.next;
-//     left = left.next;
-//   }
-//   left.next = left.next.next;
-//   return before.next;
-// };
+  header.next.next ? header.next = header.next.next :  header.next = null
+  return header
+}
